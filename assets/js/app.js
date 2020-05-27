cbb7523c255fb4feb25fb1b371f001ee6acca1be
twttin <50750373+twttin@users.noreply.github.com>
2020-05-26 22:03:48 -0700

    Website update.

--- assets/js/app.js
+++ assets/js/app.js
@@ -86,7 +86,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "bubble"
+          "mode": "grab"
         },
         "onclick": {
           "enable": true,
