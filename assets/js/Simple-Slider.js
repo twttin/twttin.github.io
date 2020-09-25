52dc9d045b842eb93b2c3aa75caa333f518d2963
twttin <50750373+twttin@users.noreply.github.com>
2020-09-25 16:50:03 -0700

    Website update.

--- assets/js/Simple-Slider.js
+++ assets/js/Simple-Slider.js
@@ -3,12 +3,11 @@ $(function(){
     // Initializing the swiper plugin for the slider.
     // Read more here: http://idangero.us/swiper/api/
     
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
+var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
     
 });
